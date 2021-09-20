f34fdd14090d985a5842941577c3e480d3899350
Johnclaudio <kidizinhu200@gmail.com>
2021-09-20 15:28:45 -0300

    Adicionando BD: ECOMMERCE E SERVICOS_RH

---
+++ BD_ECOMMERCE.sql
@@ -0,0 +1,72 @@
+/*
+Atividade 2
+Crie um banco de dados para um e commerce, onde o sistema trabalhará com as
+informações dos produtos deste ecommerce.
+Crie uma tabela produtos e utilizando a habilidade de abstração e determine 5 atributos
+relevantes dos produtos para se trabalhar com o serviço deste ecommerce.
+Popule esta tabela com até 8 dados;
+Faça um select que retorne os produtos com o valor maior do que 500.
+Faça um select que retorne os produtos com o valor menor do que 500.
+Ao término atualize um dado desta tabela através de uma query de atualização.
+salve as querys para cada uma dos requisitos o exercício em um arquivo .SQL ou texto e
+coloque no seu GitHuB pessoal e compartilhe esta atividade.
+*/
+drop database DB_ECOMMERCE;
+CREATE DATABASE IF NOT EXISTS DB_ECOMMERCE;
+USE DB_ECOMMERCE;
+
+CREATE TABLE IF NOT EXISTS tb_categoria(
+id_categoria int auto_increment not null,
+nome varchar(60)  not null,
+PRIMARY KEY (id_categoria)
+);
+INSERT IGNORE INTO tb_categoria (nome) VALUES ('MATERIAL ESCOLAR'),('PRODUTOS DE HIGIENE'), ('ELETRÔNICOS');
+
+
+CREATE TABLE IF NOT EXISTS tb_produtos(
+id_produto int auto_increment not null,
+nome varchar(100)  not null,
+descricao text,
+valor_unitario float not null,
+validade date, 
+categoria int not null,
+create_at timestamp,
+PRIMARY KEY (id_produto),
+FOREIGN KEY (categoria) REFERENCES tb_categoria(id_categoria)
+);
+
+CREATE TABLE IF NOT EXISTS tb_estoque(
+id_estoque int auto_increment not null,
+produto int not null unique,
+quantidade_estoque SMALLINT,
+PRIMARY KEY (id_estoque),
+FOREIGN KEY (produto) references tb_produtos(id_produto)
+);
+
+INSERT IGNORE INTO tb_produtos (nome,descricao,valor_unitario,validade,categoria) VALUES 
+(
+'BORRACHA',
+'Borracha Plástica Faber Castell Branca Fc Max 2unid Tk Plast',
+5.00,
+2025-09-20,
+1
+),
+
+(
+'Lápis De Cor 50 Cores',
+'Mina super macia exclusiva, garantindo o máximo de conforto ao pintar Cores mais vivas',
+40.00,
+2065-09-20,
+1
+);
+
+INSERT IGNORE INTO tb_estoque(produto,quantidade_estoque) VALUES (1,25);
+INSERT IGNORE INTO tb_estoque(produto,quantidade_estoque) VALUES (2,25);
+
+
+select * from tb_produtos;
+select * from tb_estoque;
+
+
+
+
---
+++ BD_SERVICOS_RH.sql
@@ -0,0 +1,65 @@
+/*Atividade 1
+Crie um banco de dados para um serviço de RH de uma empresa, onde o sistema
+trabalhará com as informações dos funcionaries desta empresa.
+Crie uma tabela de funcionaries e utilizando a habilidade de abstração e determine 5
+atributos relevantes dos funcionaries para se trabalhar com o serviço deste RH.
+Popule esta tabela com até 5 dados;
+Faça um select que retorne os funcionaries com o salário maior do que 2000.
+Faça um select que retorne os funcionaries com o salário menor do que 2000.
+Ao término atualize um dado desta tabela através de uma query de atualização.
+salve as querys para cada uma dos requisitos o exercício em*/
+
+drop database DB_RH;
+CREATE DATABASE IF NOT EXISTS DB_RH;
+USE DB_RH;
+
+CREATE TABLE IF NOT EXISTS tb_funcionario(
+CPF CHAR (11),
+nome varchar(100)  not null,
+nascimento date,
+uf_estado char(2)  not null, 
+salario float not null,
+setor varchar(30)  not null,
+admissao date,
+create_at timestamp,
+PRIMARY KEY (CPF)
+);
+
+INSERT IGNORE INTO tb_funcionario VALUES (
+18022060862,
+'Eowyn Haikela Souza',
+'1996-09-20',
+'SP',
+5000.20,
+'Desenvolvimento de Sistemas',
+'2019-09-20',
+default),
+
+(
+'48022060892',
+'Miguel Tulho Soares',
+'2000-05-1',
+'MG',
+3000.00,
+'Desenvolvimento de Sistemas',
+'2021-05-05',
+default
+),
+
+('49069060892',
+'André Jr',
+'2000-05-1',
+'SP',
+1500.00,
+'Estagiario desenvolvimento de Sistemas',
+'2021-05-05',
+default
+);
+
+SELECT nome,salario as SALARIO_SUPERIOR_2000 from tb_funcionario WHERE (salario > 2000) ;
+SELECT nome,salario as SALARIO_INFERIOR_2000 from tb_funcionario WHERE (salario < 2000) ;
+
+
+
+
+ 
